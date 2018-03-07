738232026cf25783e5008e9cf93cbc820fce7d56
Vivian Allen <mrvivianallen@gmail.com>
2018-03-07 18:08:37 +0000

    name adding feature red-green

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
   get "/" do
-    "Testing infrastructure working!"
+    erb :index
   end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
+  end
+
   run! if app_file == $0
+
 end
 
 # Sinatra's modulus style set up
