7db8d7f06b487bdb2a17f5e4b8671daff4a33ce4
Vivian Allen <mrvivianallen@gmail.com>
2018-03-08 11:50:13 +0000

    changed attacking players scenario spec

--- spec/features/attacking_players_spec.rb
+++ spec/features/attacking_players_spec.rb
@@ -2,6 +2,6 @@ feature 'Attacking Players' do
   scenario 'Player 2 is attacked' do
     sign_in_and_play('Bill', 'Steve')
     click_button 'ATTACK'
-    expect(page).to have_content ('Steve HP: 80')
+    expect(page).to have_content ('Bill the Bin ATTACKS Steve the Skip')
   end
 end
